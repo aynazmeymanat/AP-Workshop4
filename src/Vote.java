151887c43741f755eb5b3e78e57f24333ba5dc67
aynazmeymanat <aynazmeymanat@gmail.com>
2024-04-08 14:43:26 +0330

    2st commit by aynazmeymanat add Vote.java

--- src/Vote.java
+++ src/Vote.java
@@ -1,2 +1,32 @@
+import java.util.Objects;
+
 public class Vote {
+    private Person Voter ;
+    private String date ;
+
+    public Vote(Person voter, String date) {
+        Voter = voter;
+        this.date = date;
+    }
+
+    public Person getVoter() {
+        return Voter;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vote vote = (Vote) o;
+        return Objects.equals(Voter, vote.Voter) && Objects.equals(date, vote.date);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Voter, date);
+    }
 }
